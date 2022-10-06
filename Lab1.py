1c1347370caf362dcb23bf1b7d02a6fb7231c053
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-10-06 09:26:00 +0800

    Update Lab1.py
    Add detailed comments

--- Lab1.py
+++ Lab1.py
@@ -11,6 +11,14 @@ import configparser
 import numpy as np
 import time
 
+##################################################################################
+# Lab1 description :
+# 1. Create another thread, subscriber thread, to receive the data from publisher
+# 2. Receive 10 images from broker to determine intrinsic parameters
+# 3. Remove distortion effect from a testing image
+##################################################################################
+
+
 # MQTT subscriber thread
 class Client(threading.Thread):
     def __init__(self, broker):
@@ -18,7 +26,7 @@ class Client(threading.Thread):
 
         self.killswitch = threading.Event()
         self.broker = broker
-        print("[DBG] Sub init")
+        logging.info("[DBG] Client object initializes MQTT client setting")
 
         # TODO2: setup MQTT client
         client = mqtt.Client()
@@ -28,11 +36,11 @@ class Client(threading.Thread):
         self.client = client
 
         # TODO3: set topic you want to subscribe
-        self.topic = "server_response_7"    #"test/topic/topic"
+        self.topic = "server_response_7"
         self.topic_2 = "secret_photo_7"
 
     def on_connect(self, client, userdata, flag, rc):
-        print(f"Connected with result code: {rc}")
+        logging.info(f"Connected with result code: {rc}")
         self.client.subscribe(self.topic)
         self.client.subscribe(self.topic_2)
 
@@ -44,17 +52,17 @@ class Client(threading.Thread):
 
         if 'data' in cmds:
             os.makedirs('./q2', exist_ok=True)
-            print("[DBG1]")
+            logging.info(f"[DBG] begin receiving 10 images... ...")
             for i in range(len(cmds['data'])):
-                print("[DBG] ")
                 # TODO4: save iamge to ./q2, so you have to decode msg
                 image = coverToCV2(cmds['data'][i])
                 filename = 'photo_' + str(i) + '.png'
                 cv2.imwrite(os.path.join('./q2', filename), image)
+                logging.info(f"[DBG] R/W {filename}")
 
         if 'dist_data' in cmds:
-            print("[DBG] dist_data")
-#            # TODO5: you have to decode msg
+            logging.info("[DBG] begin receiving a testing distorted image... ...")
+            # TODO5: you have to decode msg
             image = coverToCV2(cmds['dist_data'])
             cv2.imwrite('dist_image.png', image)
 
@@ -62,21 +70,26 @@ class Client(threading.Thread):
         self.killswitch.set()
 
     def run(self):
+        logging.info("[DBG] Subscriber thread starts to run")
         try:
-            print("[DBG] run")
             self.client.loop_start()
-            print("[DBG] wait")
+            logging.info("[DBG] Subscriber thread is blocking for event...")
             self.killswitch.wait()
-            print("[DBG] ready")
         finally:
             self.client.loop_stop()
 
+
 # use this function to decode msg from server
+# [Description]
+# This function will convert the received string (i.e. data) to bytes by base64.b64decode()
+# Then, use pickle’s load method to convert from a bytes-array type back to the original object
+# Last, read an image from a buffer in memory
 def coverToCV2(data):
     imdata = base64.b64decode(data)
     buf = pickle.loads(imdata)
     image = cv2.imdecode(buf, cv2.IMREAD_COLOR)
     return image
+
 
 # save config to './config'
 def saveConfig(cfg_file, config):
@@ -87,11 +100,14 @@ def saveConfig(cfg_file, config):
         logging.error(e)
         sys.exit()
 
+
 # load config from './config'
+# [Description]
+# This function will read ./config as dict type by configparser.ConfigParser()
 def loadConfig(cfg_file):
     try:
         config = configparser.ConfigParser()
-        config.optionxform = str
+        config.optionxform = str      # This will let [Intrinsic] be case-sensitive, but it seems always case-sensitive
         with open(cfg_file) as f:
             config.read_file(f)
     except IOError as e:
@@ -99,6 +115,12 @@ def loadConfig(cfg_file):
         sys.exit()
     return config
 
+# [Description]
+# This function will determine intrinsic parameters of camera
+# First, define the real world coordinates of each corners
+# Second, use cv2.findChessboardCorners() to get coordinates of corners in the images
+# Third, determine intrinsic parameters of camera
+# Fourth, set alpha=0 to get an undistorted image with minimum unwanted pixels, and also get new intrinsic parameters for that new image
 def calculateIntrinsic():
     config_file = './config'
     cameraCfg = loadConfig(config_file)
@@ -106,16 +128,20 @@ def calculateIntrinsic():
     # calculate mtx, dist, newcameramtx here
     # ================================
 
+    # threhold
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
     # TODO11: set corner number
     corner_x = 7
     corner_y = 7
 
-    objp = np.zeros((corner_x*corner_y,3), np.float32)
-    objp[:,:2] = np.mgrid[0:corner_x, 0:corner_y].T.reshape(-1,2)
+    objp = np.zeros((corner_x * corner_y, 3), np.float32)                # A 2-D float-32 type array [49, 3] with initialized values all 0
+    objp[:, :2] = np.mgrid[0:corner_x, 0:corner_y].T.reshape(-1, 2)      # Here we define the real world coordinates for corners (Z coordinate is 0)
+                                                                         # 2x7x7 ==> Transpose ==> 7x7x2 ==> reshape ==> 49x2
 
-    objpoints = [] # 3d points in real world space
-    imgpoints = [] # 2d points in image plane.
-    images = []
+    objpoints = []  # 3d points in real world space
+    imgpoints = []  # 2d points in image plane.
+    images = []  # Store file names of 10 images (photo_0.png, etc...)
     for filename in os.listdir('./q2'):
         fullpath = os.path.join('./q2', filename)
         if filename.endswith('.png'):
@@ -128,26 +154,48 @@ def calculateIntrinsic():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # TODO13: find image point by opencv
+        # If find desired number of corners, then ret will be True; otherwise, will be false
+        # In this lab case, it should be 7x7 = 49
         ret, corners = cv2.findChessboardCorners(gray, (corner_x, corner_y), None)
-
+        #print(corners[0])
         if ret == True:
+            ## @TODO:for more accurate
+            refined_corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            print(refined_corners)
+            ##
             objpoints.append(objp)
             imgpoints.append(corners)
+            # @TODO: for more accurate
+            #imgpoints.append(refined_corners)
 
+
+    # width  => img.shape[1]
+    # height => img.shape[0]
     img_size = (img.shape[1], img.shape[0])
 
     # TODO14: get camera matrix and dist matrix by opencv
+    # Camera is fixed, so get intrinsic matrix, lens distortion coef. and rotation/translation vectors
+    # Actually, I have no idea why we don't need to use rvecs and tvecs
+    # Even if the official tutorial doesn't describe
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
     # TODO15: get optimal new camera matrix, alpha = 0
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, img_size, 0, img_size)
+    # Here we use the new image size as the same size of img_size
+    # Also, we set alpha=0 to have an undistorted image with minimum unwanted pixels
+    # BTW, if we set alpha=1, then we can crop the image from roi information
+    new_img_size = img_size
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, img_size, 0, new_img_size)
     # ================================
 
     cameraCfg['Intrinsic']['ks'] = str(mtx.tolist())
     cameraCfg['Intrinsic']['dist'] = str(dist.tolist())
     cameraCfg['Intrinsic']['newcameramtx'] = str(newcameramtx.tolist())
     saveConfig(config_file, cameraCfg)
+    return objpoints, imgpoints, img_size
 
+
+# [Description]
+# This function removes distort effect
 def undistImage():
     config_file = './config'
     cameraCfg = loadConfig(config_file)
@@ -165,49 +213,69 @@ def undistImage():
 
     output_name = 'D_' + image
     cv2.imwrite(output_name, dst)
+
     # ================================
+
 
 def sendMsg(broker):
     # TODO6: setup MQTT client
     client = mqtt.Client()
     client.connect(broker)
-    topic = "deep_learning_lecture_7"      #"test/topic/topic"
+    topic = "deep_learning_lecture_7"
 
     # TODO7: send msg to get echo
     payload = {'text': 'test hello'}
+    logging.info(f'[DBG] To get echo => sendMsg:{payload}')
     client.publish(topic, json.dumps(payload))
 
     # TODO8: send msg to get hint
     payload = {'request': 'photo'}
     client.publish(topic, json.dumps(payload))
+    logging.info(f'[DBG] To get hint => sendMsg:{payload}')
+    # receive the hint message
+    # "subscribe topic [secret_photo], send msg [request] for [EC234_NOL], and receive [data] to get 10 photo"
 
     # TODO9: send msg to get 10 photos
     payload = {'request': 'EC234_NOL'}
     client.publish(topic, json.dumps(payload))
+    logging.info(f'[DBG] To get 10 images => sendMsg:{payload}')
 
     # TODO10: send msg to get 1 photo to undistort
     payload = {'request': 'dist_photo'}
     client.publish(topic, json.dumps(payload))
+    logging.info(f'[DBG] To get 1 testing distorted image => sendMsg:{payload}')
+
+
+def calculateTotalError(objpoints, imgpoints, img_size):
+    # get camera matrix and dist matrix by opencv
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+    total_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        total_error += error
+    print("total error:", total_error / len(objpoints))
+
 
 def main():
     # start mqtt subscribe thread
     # ================================
     # TODO1: set broker ip to connect
-    broker_ip = "140.113.208.103"#"test.mosquitto.org"
-    mainThread = Client(broker_ip)   # Create a new thread
-    mainThread.start()   # Start running thread
+    broker_ip = "140.113.208.103"
+    mainThread = Client(broker_ip)  # Create a new thread
+    mainThread.start()              # Start running thread
     # ================================
 
     # send msg to server
     # ================================
-    sendMsg(broker_ip)
+    sendMsg(broker_ip)              # Another thread will be Publisher to send the data to broker
     # ================================
 
     time.sleep(3)
 
     # calculate intrinsic matrix and save in config file
     # ================================
-    calculateIntrinsic()
+    objpoints, imgpoints, img_size = calculateIntrinsic()
     # ================================
 
     # undistortion image
@@ -215,7 +283,14 @@ def main():
     undistImage()
     # ================================
 
+    calculateTotalError(objpoints, imgpoints, img_size)
+
     mainThread.join()
 
+
 if __name__ == '__main__':
+    # Set debug level
+    # Print logging.info by level=logging.INFO
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
     main()
